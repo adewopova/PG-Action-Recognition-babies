6131814ef572e83d68f5cefd0253a7f966e2611c
adewopova <59388844+adewopova@users.noreply.github.com>
2021-05-25 00:39:43 -0400

    Update firstapp.py

--- firstapp.py
+++ firstapp.py
@@ -12,97 +12,160 @@ import os
 import tempfile
 import ssl
 import math
-session_conf = tf.ConfigProto(
-      intra_op_parallelism_threads=1,
-      inter_op_parallelism_threads=1)
-sess = tf.Session(config=session_conf)
+
+PATH=os.getcwd()
+vidup= st.file_uploader('Upload Your Video')
+
+
 st.write('Loading saved model') 
 
 #Load Saved Model
-PATH=os.getcwd()
-K_I_512D=tf.keras.models.load_model(PATH +'/saved_model/5sec_AR_kineticsweightsonly_noflatten_moredense.h5')
-#K_I_aug_20E= tf.keras.models.load_model(PATH+'/saved_model/augumented_5sec_AR_kinetics+ImageNetweightsonly.h5')
-#K_3DL= tf.keras.models.load_model(PATH+'/saved_model/5sec_AR_kineticsweightsonly_noflatten_moredense.h5')
-# Check its architecture
+#@st.cache
+def K_I_512D(videopath):
+    K_I_512D=tf.keras.models.load_model(PATH +'/saved_model/5sec_AR_kineticsweightsonly_noflatten_moredense.h5')
+    v=K_I_512D.predict(videopath)
+    return v
+#@st.cache
+def K_I_aug_20E(videopath):
+    K_I_aug_20E=tf.keras.models.load_model(PATH +'/saved_model/augumented_5sec_AR_kinetics+ImageNetweightsonly.h5')
+    v=K_I_aug_20E.predict(videopath)
+    return v
+#@st.cache
+def K_3DL(videopath):
+    K_3DL=tf.keras.models.load_model(PATH +'/saved_model/5sec_AR_kineticsweightsonly_noflatten_moredense.h5')
+    v=K_3DL.predict(videopath)
+    return v
 
-st.write('All loaded') 
-
-vidup= st.file_uploader('Upload Your Video')
-def vid_read(vid):
-    vid_feed=vid.read()
-    return vid_feed
-
-@st.cache
-def video_test(vidup):
-
-    def crop_center_square(frame):
-        (y, x) = frame.shape[0:2]
-        min_dim = min(y, x)
-        start_x = x // 2 - min_dim // 2
-        start_y = y // 2 - min_dim // 2
-        return frame[start_y:start_y + min_dim, start_x:start_x
-                     + min_dim]
-
-    max_frames = 150
-    vidhold = tempfile.NamedTemporaryFile(delete=False)
-    vidhold.write(vid_read(vidup))
+st.write('All models loaded') 
     
-    cap = cv2.VideoCapture(vidhold.name)
-    #cap = cv2.VideoCapture(video_paths)
-    #frames = []
-    frames = np.zeros(shape=(max_frames, 224, 224, 3))
-    i =0
-    try:
-        while True:
-            (ret, frame) = cap.read()
-            if not ret:
-                break
-            frame = crop_center_square(frame)
-            frame = cv2.resize(frame, (224, 224))
-            frame = frame[:, :, [2, 1, 0]]
 
-            frames[i]=frame
-            i+=1
-            if i==150:
-                break
-#                 frames.append(frame)
+#For streamlt
+#Instant prediction without trimnming or saving the junks
+def all_embed(videopath):
+    
+    def dummy_get_totalframes_fps(videopath):      #Get duration of clips function #Changing this script to read fs and total 
+        #frames at once because of stream lit bug that wants me to realodd the page twice before getting them
+        clip= cv2.VideoCapture(videopath)
+        totalframes = int(clip.get(cv2.CAP_PROP_FRAME_COUNT))
+        fps = int(clip.get(cv2.CAP_PROP_FPS))
+        totalframes=int(totalframes)
+        return[totalframes, fps, clip]
+    def get_totalframes(path):
+        tframes=Frame_and_fps[0]
+        return tframes
+    def get_5secs(path):
+        fps=Frame_and_fps[1]
+        return fps
+    #@st.cache(allow_output_mutation=True)
+    def get_clip(path):
+        clip=Frame_and_fps[2]
+        return clip
+    
+    
+    def get_range(framestart,frameend,vidfps): #This function to get prediction range
+        prediction_range=('{} - {}'.format(math.ceil(framestart/vidfps),math.ceil(frameend/vidfps)))
+        #st.write('\n')
+        st.write('Action Detected for %s seconds by the three models are'%(prediction_range))
+        st.write('------------------------------------------------------------------') 
 
-#                 if len(frames) == max_frames:
-#                     break
-    finally:
-        cap.release()
-    #yield np.array(frames) / 255.0, Label
-    return (tf.constant(frames, dtype=tf.float32)[tf.newaxis, ...])/ 255.0
+    def video_test(videopath,framestart,frameend): ##Instant prediction without trimnming or saving the junks
 
+        def crop_center_square(frame):
+            (y, x) = frame.shape[0:2]
+            min_dim = min(y, x)
+            start_x = x // 2 - min_dim // 2
+            start_y = y // 2 - min_dim // 2
+            return frame[start_y:start_y + min_dim, start_x:start_x
+                         + min_dim]
 
+        cap = cv2.VideoCapture(videopath)
+        max_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        frames = []
+        frames = np.zeros(shape=(max_frames, 224, 224, 3))
+        i=0
+        try:
+            while True:
+                (ret, frame) = cap.read()
+                if not ret:
+                    break
+                frame = crop_center_square(frame)
+                frame = cv2.resize(frame, (224, 224))
+                frame = frame[:, :, [2, 1, 0]]
 
-def to_gif(images):
-    #Code to see the videos that are loaded in Gif format. pass in the output of generator. The np.array only
-    converted_images =np.clip(images.numpy()[0]* 255, 0, 255).astype(np.uint8)
-    #gifnum=len([f for f in glob.glob(PATH+"**/*.gif", recursive=True)])+1
-    imageio.mimsave('./animation.gif', converted_images, fps=25)
-    #IPython.display.Image('animation.gif')
-    return embed.embed_file('./animation.gif')
-    #![SegmentLocal](191px-Seven_segment_display-animation.gif "segment")
+                frames[i]=frame
+                i+=1
+                if i==max_frames:
+                    break
 
+        finally:
+            cap.release()
+        newframes1=frames[framestart:frameend]
+        newframes=np.pad(newframes1, ((0,150-len(newframes1)),(0,0),(0,0),(0,0)), mode='constant')
+        return (tf.constant(newframes, dtype=tf.float32)[tf.newaxis, ...])/ 255.0
+    
+    def modelpredict(clip): #Get the model output for all 5sec junk
+       # print(clip.shape)
+        labels=np.array(['on_feet', 'active', 'rest', 'escape', 'crawling'])
+        K_I_512D_result=K_I_512D((clip))[0]
+        probabilities1 = tf.nn.softmax(K_I_512D_result).numpy()
+        probabilities1=np.array(probabilities1)
+        #st.write('------------------------------------------------------------------') 
+        
+        for i in np.argsort(probabilities1)[::-1][:1]:
+            st.write("The Kinetics and Imagenet 512D model \t\t-->{}".format(f"  {labels[i]:}: {probabilities1[i] * 100:5.2f}%"))
+            #st.write(f"  {labels[i]:}: {probabilities1[i] * 100:5.2f}%")
+        #st.write('------------------------------------------------------------------') 
+        K_3DL_result= K_3DL((clip))[0]
+        probabilities2 = tf.nn.softmax(K_3DL_result)
+        
+        for i in np.argsort(probabilities2)[::-1][:1]:
+            st.write("The Kinetics 3DL model                \t\t\t -->{}".format(f"  {labels[i]:}: {probabilities2[i] * 100:5.2f}%"))
+            #st.write(f"  {labels[i]:}: {probabilities2[i] * 100:5.2f}%")
+        #st.write('------------------------------------------------------------------') 
+        K_I_aug_20E_result= K_I_aug_20E((clip))[0]
+        probabilities3 = tf.nn.softmax(K_I_aug_20E_result)
+        
+        for i in np.argsort(probabilities3)[::-1][:1]:
+            st.write("The Kinetics and Imagenet video_augmented model   \t -->{}".format(f"  {labels[i]:}: {probabilities3[i] * 100:5.2f}%"))
+            #st.write(f"  {labels[i]:}: {probabilities3[i] * 100:5.2f}%")
+        st.write('------------------------------------------------------------------')
+    
+    def split_frames(videopath): # breaks all long video into short frames and prints the results
+        tframes=get_totalframes(videopath)
+        fivesecs=get_5secs(videopath)*5
+        vidfps=get_5secs(videopath)
+        divisorrate=math.ceil(tframes/fivesecs) #Get the divsion rate to know how many number of iterations will be done on the video
+        framestart=0
+        frameend=fivesecs
+        st.write('The uploaded video will be splitted to :', divisorrate,'chunks of 5-seconds each.')
+        st.write('------------------------------------------------------------------') 
+        for i in range(divisorrate):
+            if tframes-frameend>fivesecs:
+                get_range(framestart,frameend,vidfps)
+                modelpredict(video_test(videopath,framestart,frameend))
+            elif tframes-frameend<0:
+                framestart=(tframes-fivesecs)
+                frameend=(framestart+fivesecs)
+                get_range(framestart,frameend,vidfps)
+                modelpredict(video_test(videopath,framestart,frameend))
+            else:
+                framestart=(tframes-fivesecs)-(tframes-frameend)
+                get_range(framestart,frameend,vidfps)
+                modelpredict(video_test(videopath,framestart,frameend))
+            framestart=frameend
+            frameend=framestart+fivesecs
+    Frame_and_fps=dummy_get_totalframes_fps(videopath)       
+    split_frames(videopath)
+    st.video(videopath)
 if vidup is not None:
+    videofeed=vidup.read()
+    vidhold=tempfile.NamedTemporaryFile(delete=False)
+    vidhold.write(videofeed)
     upload_details={'video_type':vidup.type,'video_name':vidup.name}
     st.write(upload_details)
 
-    
-    st.write('About running Prediction')   
-    labels=['on_feet', 'active', 'rest', 'escape', 'crawling']
-    clip=video_test(vidup) #FUNCTION TO crop and preprocess video
-    output1= K_I_512D.predict(clip)
-    #print(output1)
-
-    kin_RGB_k_Iresult=output1[0]
-    #print(kin_RGB_k_Iresult)
-    probabilities1 = tf.nn.softmax(kin_RGB_k_Iresult)
-    #print(kin_RGB_k_Iresultresult)
-    st.write("The Kinetics and Imagenet model topmost three predictions are:")
-    for i in np.argsort(probabilities1)[::-1][:3]:
-        st.write(f"  {labels[i]:}: {probabilities1[i] * 100:5.2f}%")
-        #st.image(to_gif(clip))
-    #video_byte = vidup.read()
-    st.video(vid_read(vidup))#video_byte)
+    videopath=vidhold.name
+    all_embed(videopath)
+    for files in os.listdir(PATH+'/tempfile/'):
+        os.remove(PATH+'/tempfile/'+files)
+   
